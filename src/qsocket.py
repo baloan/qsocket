54be1dec65d8df768409bf1f350e1ea9871f6828
Andreas Balogh <baloand@gmail.com>
2016-09-10 18:59:24 +0200

    test cases for connection loss added

--- src/qsocket.py
+++ src/qsocket.py
@@ -7,69 +7,79 @@ import select
 import socket
 import struct
 from threading import Thread, Lock, Event
+from pprint import pprint
+import sys
+
+SELECT_TIMEOUT = 1.0
 
 
 def create_qsocket(addr):
     socket = socket.create_connection(addr)
-    qs = QSocket(socket)
-    qs.start()
-    return qs
+    return QSocket(socket)
 
 
 class QSocket():
     """ Threadsafe socket connection """
+    PACK_FMT = "!i"
+    PACK_SIZE = struct.calcsize(PACK_FMT)
     # https://docs.python.org/3/howto/sockets.html#socket-howto
 
     def __init__(self, sock):
         self.inq = Queue()
         self.sock = sock
+        # self.sock.settimeout(1.0)
         self.threads = {}
         self.sock_lock = Lock()
+        self.terminate = Event()
         self.pump = Thread(target=self.receive, daemon=True)
-        self.terminate = False
-
-    def start(self):
         self.pump.start()
 
     def send(self, obj):
+        if obj is None:
+            raise ValueError()
         obj_bytes = pickle.dumps(obj)
         # max size of "i" is 4 bytes i.e. 2GB
-        obj_len = struct.pack("i", len(obj_bytes))
-        # lock socket only while sending
+        obj_len = struct.pack(QSocket.PACK_FMT, len(obj_bytes))
+        buffer = b''.join((obj_len, obj_bytes))
+        # pprint(buffer)
         with self.sock_lock:
-            print("Sending {} bytes...".format(len(obj_bytes)))
-            self.sock.sendall(obj_len + obj_bytes)
+            self.sock.sendall(buffer)
 
-    def close(self):
-        self.terminate = True
+    def close(self, wait=False):
+        self.terminate.set()
+        if wait:
+            self.pump.join()
 
     def receive(self):
-        size_header = struct.calcsize("i")
         try:
-            while not self.terminate:
+            while not self.terminate.is_set():
                 # timeout wait for data to become available
-                sread, _, _ = select.select([self.sock], [], [], 1)
+                sread, _, _ = select.select([self.sock], [], [], SELECT_TIMEOUT)
                 if sread != []:
-                    # lock socket only while receiving
-                    with self.sock_lock:
-                        print("Receiving...")
-                        b = self.recv_bytes(size_header)
-                        if b is None:
-                            break
-                        obj_len = struct.unpack("i", b)[0]
-                        print("Receiving {} bytes".format(obj_len))
-                        obj_bytes = self.recv_bytes(obj_len)
+                    len_bytes = self.recv_bytes(QSocket.PACK_SIZE)
+                    obj_len = struct.unpack(QSocket.PACK_FMT, len_bytes)[0]
+                    obj_bytes = self.recv_bytes(obj_len)
                     obj = pickle.loads(obj_bytes)
                     self.process(obj)
+        except OSError as e:
+            print(e, file=sys.stderr)
         finally:
-            self.sock.shutdown(socket.SHUT_RDWR)
-            self.sock.close()
+            with self.sock_lock:
+                self.sock.shutdown(socket.SHUT_RDWR)
+                self.sock.close()
+            self.process(None)
 
     def recv_bytes(self, n):
         chunks = []
         received = 0
         while received < n:
-            chunk = self.sock.recv(n - received)
+            # add a timeout to protect again partly frozen source
+            # lock socket only while receiving
+            with self.sock_lock:
+                chunk = self.sock.recv(n - received)
+            if len(chunk) == 0:
+                raise BrokenPipeError("[{}] [{}] Remote socket closed, Local {}, Remote {}".format(
+                    self.pump.name, self.pump.ident, self.sock.getsockname(), self.sock.getpeername()))
             chunks.append(chunk)
             received = received + len(chunk)
         return b''.join(chunks)
@@ -81,25 +91,26 @@ class QSocket():
 class Listener(Thread):
 
     def __init__(self, addr=("", 8080), socket_class=QSocket):
-        Thread.__init__(self)
+        Thread.__init__(self, name="Port-{}".format(addr[1]))
         self.addr = addr
         self.socket_class = socket_class
         self.sockq = Queue()
-        self.terminate = False
+        self.terminate = Event()
 
     def run(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            print("Binding to {}".format(self.addr))
             s.bind(self.addr)
-            s.listen(2)
-            while not self.terminate:
+            s.listen()
+            while not self.terminate.is_set():
                 # timeout wait for connection to become available
-                sread, _, _ = select.select([s], [], [], 1)
+                sread, _, _ = select.select([s], [], [], SELECT_TIMEOUT)
                 if sread != []:
                     sock, _ = s.accept()
                     qs = self.socket_class(sock)
-                    qs.start()
                     self.sockq.put(qs)
+        self.sockq.put(None)
 
-    def close(self):
-        self.terminate = True
+    def close(self, wait=False):
+        self.terminate.set()
+        if wait:
+            self.join()
--- test/test_qsocket.py
+++ test/test_qsocket.py
@@ -7,37 +7,135 @@ from pprint import pprint
 import unittest
 import socket
 from qsocket import QSocket, Listener
+from time import sleep
+import threading
 
 
 class Echo(QSocket):
+
     def __init__(self, socket):
         QSocket.__init__(self, socket)
 
     def process(self, obj):
-        self.send(obj)
+        if obj is not None:
+            self.send(obj)
 
 
-class PassiveEchoServer(unittest.TestCase):
-    def setUp(self):
-        addr = ("", 8010)
-        self.listener = Listener(addr, Echo)
-        self.listener.start()
+class EchoServer(unittest.TestCase):
 
-    def tearDown(self):
-        print("Close listener socket {}".format(self.listener))
-        self.listener.close()
+    def test_string(self):
+        port = 8020
+        laddr = ("", port)
+        raddr = ("127.0.0.1", port)
+        listener = Listener(laddr, Echo)
+        listener.start()
+        sock = socket.create_connection(raddr)
+        alice = QSocket(sock)
+        bob = listener.sockq.get()
+        print("Running threads:")
+        print("Main     {}".format(threading.current_thread()))
+        print("Listener {}".format(listener))
+        print("Alice    {}".format(alice.pump))
+        print("Bob      {}".format(bob.pump))
+        req = "Hello echo!"
+        alice.send(req)
+        resp = alice.inq.get()
+        self.assertEqual(req, resp)
+        listener.close()
+        alice.close()
+        bob.close()
 
-    def test_echo(self):
-        addr = ("127.0.0.1", 8010)
-        sock = socket.create_connection(addr)
-        print("Connected to {}".format(sock))
-        qs = QSocket(sock)
-        qs.start()
-        qs.send("Hello world!")
-        buf = qs.inq.get()
-        print(buf)
-        echo_sock = self.listener.sockq.get()
-        print("Close echo socket {}".format(echo_sock))
-        echo_sock.close()
-        print("Close active socket {}".format(qs))
-        qs.close()
+
+class SocketClose(unittest.TestCase):
+
+    def test_baseline(self):
+        port = 8021
+        laddr = ("", port)
+        raddr = ("127.0.0.1", port)
+        # Server
+        listener = Listener(laddr)
+        listener.start()
+        # Client
+        sock = socket.create_connection(raddr)
+        alice = QSocket(sock)
+        # Bob open connection
+        bob = listener.sockq.get()
+        print("Running threads:")
+        print("Main     {}".format(threading.current_thread()))
+        print("Listener {}".format(listener))
+        print("Alice    {}".format(alice.pump))
+        print("Bob      {}".format(bob.pump))
+        # Alice sends an update
+        req = {"action": "subscribe", "name": "foo", }
+        alice.send(req)
+        resp = bob.inq.get()
+        self.assertEqual(req, resp)
+        # Bob sends an update
+        req = {"action": "update", "name": "foo", "value": 120.2}
+        bob.send(req)
+        resp = alice.inq.get()
+        self.assertEqual(req, resp)
+        # close all sockets
+        bob.close()
+        alice.close()
+        listener.close()
+
+    def test_remote_close(self):
+        port = 8022
+        laddr = ("", port)
+        raddr = ("127.0.0.1", port)
+        # Server
+        listener = Listener(laddr)
+        listener.start()
+        # Client
+        sock = socket.create_connection(raddr)
+        alice = QSocket(sock)
+        # Bob open connection
+        bob = listener.sockq.get()
+        print("Running threads:")
+        print("Main     {}".format(threading.current_thread()))
+        print("Listener {}".format(listener))
+        print("Alice    {}".format(alice.pump))
+        print("Bob      {}".format(bob.pump))
+        # Alice sends an update
+        req = {"action": "create", "name": "foo", }
+        alice.send(req)
+        resp = bob.inq.get()
+        self.assertEqual(req, resp)
+        bob.close(wait=True)
+        # Alice sends an update
+        req = {"action": "update", "name": "foo", "value": 120.2}
+        self.assertRaises(OSError, alice.send, req)
+        # close all sockets
+        alice.close()
+        listener.close()
+
+    def test_local_close(self):
+        port = 8023
+        laddr = ("", port)
+        raddr = ("127.0.0.1", port)
+        # Server
+        listener = Listener(laddr)
+        listener.start()
+        # Client
+        sock = socket.create_connection(raddr)
+        alice = QSocket(sock)
+        # Bob open connection
+        bob = listener.sockq.get()
+        print("Running threads:")
+        print("Main     {}".format(threading.current_thread()))
+        print("Listener {}".format(listener))
+        print("Alice    {}".format(alice.pump))
+        print("Bob      {}".format(bob.pump))
+        # Alice sends an update
+        req = {"action": "create", "name": "foo", }
+        alice.send(req)
+        resp = bob.inq.get()
+        self.assertEqual(req, resp)
+        alice.close(wait=True)
+        # Bob sends an update
+        req = {"action": "update", "name": "foo", "value": 120.2}
+        self.assertRaises(OSError, bob.send, req)
+        # close all sockets
+        bob.close()
+        listener.close()
